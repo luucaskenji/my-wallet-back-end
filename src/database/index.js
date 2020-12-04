4dfb38a6d1a0ababc49a354ced049be419986da4
Lucas Murakami <lkenjim98@gmail.com>
2020-12-04 19:56:25 -0400

    .env.example file

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,11 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const connectionToDB = new Pool({
-    connectionString: process.env.DATABASE_URL
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
 });
 
 module.exports = { connectionToDB };
