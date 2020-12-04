fb6240013debeea3e20586af7d29fd653c780800
Lucas Murakami <lkenjim98@gmail.com>
2020-12-04 11:16:54 -0400

    GET user's finances route automated tests

--- __tests__/controllers/financesController.test.js
+++ __tests__/controllers/financesController.test.js
@@ -46,7 +46,48 @@ afterAll(async () => {
     connectionToDB.end();
 });
 
-describe('postIncome', () => {    
+describe("get user's operation", () => {
+    it("should return 200 if user's data is correctly sent", async () => {
+        const token = await getValidToken();
+
+        const response = await supertest(app)
+            .get('/finances/user-operations')
+            .set({Authorization: `Bearer ${token}`})
+
+        expect(response.status).toBe(200);
+    });
+
+    it('should return 401 if Authorization header is not sent', async () => {
+        const body = {
+            value: '11,90',
+            description: 'automated test',
+            type: 'Income'
+        };
+
+        const response = await supertest(app).post('/finances/new-operation').send(body);
+
+        expect(response.status).toBe(401);
+    });
+
+    it('should return 403 if an invalid token is sent', async () => {
+        const token = 'aacaba9-aascns1az-adfm2mqpq';
+
+        const body = {
+            value: '11,90',
+            description: 'automated test',
+            type: 'Expense'
+        };
+
+        const response = await supertest(app)
+            .post('/finances/new-operation')
+            .set({Authorization: `Bearer ${token}`})
+            .send(body);
+
+        expect(response.status).toBe(403);
+    });
+});
+
+describe('post new operation', () => {    
     it('should return 201 for correct inputs', async () => {
         const token = await getValidToken();
 
