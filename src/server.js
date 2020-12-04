9d061a2b61c5653ce4623d8366079616e0987cfa
Lucas Murakami <lkenjim98@gmail.com>
2020-12-04 17:29:05 -0400

    heroku deploy

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const { app } = require('./app');
 
-app.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on port ${process.env.PORT}`);
 });
