5784b607c620ae370560be99a57e1cdfd2d4e2a6
Lucas Murakami <lkenjim98@gmail.com>
2020-12-04 18:05:00 -0400

    heroku: updating source database

--- src/database/index.js
+++ src/database/index.js
@@ -2,11 +2,7 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const connectionToDB = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD
+    connectionString: process.env.DATABASE_URL
 });
 
 module.exports = { connectionToDB };
