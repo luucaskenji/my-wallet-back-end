1bf012e585d1ac0c1c8dc552b1381a7c921f883f
Lucas Murakami <lkenjim98@gmail.com>
2021-02-15 15:05:24 -0300

    handling app erros method

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+require('express-async-errors');
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -5,12 +8,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const dataValidation = require('./middlewares/dataValidation');
-const { authMiddleware } = require('./middlewares/auth');
-const usersRouter = require('./routers/users');
-const financesRouter = require('./routers/finances');
+const usersRouter = require('./routers/usersRouter');
+const financesRouter = require('./routers/financesRouter');
 
 app.use('/users', usersRouter);
 app.use('/finances', financesRouter);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+
+  res.status(500).send(error);
+})
 
 module.exports = app;
