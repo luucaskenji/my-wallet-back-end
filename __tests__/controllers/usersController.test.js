a26eb74feec5654ef1c8e224b196d6747f8e64f1
Lucas Murakami <lkenjim98@gmail.com>
2021-02-21 03:55:15 -0300

    users controller sign in method unit tests

--- __tests__/controllers/usersController.test.js
+++ __tests__/controllers/usersController.test.js
@@ -1,12 +1,22 @@
 const usersController = require('../../src/controllers/usersController');
 const User = require('../../src/models/User');
-const { ExistingUserError } = require('../../src/errors');
+const Session = require('../../src/models/Session');
+const { ExistingUserError, NotFoundError, WrongPasswordError } = require('../../src/errors');
 
-jest.mock('../../src/utils/sanitizer', () => array => array)
+jest.mock('../../src/utils/sanitizer', () => array => array);
 jest.mock('../../src/models/User');
+jest.mock('../../src/models/Session');
+
 jest.mock('bcrypt', () => ({
-  hashSync: (string, number) => string
+  hashSync: (string, number) => string,
+  compareSync: (password, passwordHash) => password === passwordHash
 }));
+
+jest.mock('jsonwebtoken', () => ({
+  sign: (object, string) => '123'
+}));
+
+jest.spyOn(usersController, '_findByEmail');
 
 describe('method signUp', () => {
   it('should return user data if passed email is not conflicting', async () => {
@@ -32,10 +42,51 @@ describe('method signUp', () => {
     const email = 'test@gmail.com';
     const password = '123456';
 
-    User.findOrCreate.mockResolvedValueOnce([{}, true]);
+    User.findOrCreate.mockResolvedValueOnce([{}, false]);
 
     const testFn = usersController.signUp(name, email, password);
 
     expect(testFn).rejects.toThrow(ExistingUserError);
+  });
+});
+
+describe('method signIn', () => {
+  it('should create session and return its data if user exists and correct password is sent', async () => {
+    const email = 'test@gmail.com';
+    const password = '1234';
+
+    usersController._findByEmail.mockResolvedValueOnce({ id: 1, name: 'Test', password: '1234' });
+    Session.create.mockResolvedValueOnce({ id: 2 });
+
+    const response = await usersController.signIn(email, password);
+
+    expect(Session.create).toHaveBeenCalledWith({ userId: 1 });
+    expect(response).toMatchObject({
+      name: 'Test',
+      email,
+      token: '123'
+    });
+  });
+
+  it('should throw NotFoundError if invalid email is sent', async () => {
+    const email = 'test@gmail.com';
+    const password = '1234';
+
+    usersController._findByEmail.mockResolvedValueOnce(null);
+
+    const testFn = usersController.signIn(email, password);
+
+    expect(testFn).rejects.toThrow(NotFoundError);
+  });
+
+  it('should throw WrongPasswordError if wrong password is sent', async () => {
+    const email = 'test@gmail.com';
+    const password = 'wrongPassword';
+
+    usersController._findByEmail.mockResolvedValueOnce({ id: 1, name: 'Test', password: '1234' });
+
+    const testFn = usersController.signIn(email, password);
+
+    expect(testFn).rejects.toThrow(WrongPasswordError);
   });
 });
